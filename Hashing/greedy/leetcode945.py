35a15cba73993f5d79c755f32697888decc19c7e
Diwyanshu kumar <diwyanshusvnit@gamil.com>
2026-08-28 15:20:31 +0530

    Dp

--- Hashing/greedy/leetcode945.py
+++ Hashing/greedy/leetcode945.py
@@ -3,7 +3,9 @@ class Solution(object):
         nums.sort()
         summ=0
         prev=nums[0]
-        
+        # sort the array then the element will be arranged  
+        # if [1,1,3,4] then when we changed by any array sum of final array answer will be same but if want to do 
+        # minimum no of elemnt to affect then size-len(hashmap) 
         for i in range(1,len(nums)):
             if prev<nums[i]:
                 summ+=0
